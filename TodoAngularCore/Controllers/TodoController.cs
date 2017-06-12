4a4b89c88fb6a4f0780eefe98ea885cf7236adce
Gietson <gietson1@gmail.com>
2017-06-12 20:29:29 +0200

    first commit

---
+++ TodoAngularCore/Controllers/TodoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoAngularCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "ser", "pieczarki" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TodoAngularCore/Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TodoAngularCore.Models
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        public bool Complete { get; set; }
+    }
+}
